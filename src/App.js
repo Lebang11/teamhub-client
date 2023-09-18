39dd92545059a2535df702f8118fc23121441073
L.Nong <lnong023@student.wethinkcode.co.za>
2023-09-18 09:48:30 +0200

    added mainpage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import OpenPage from './opening';
 import Register from './register';
 import Login from './login';
+import MainPage from './main';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <Route path='/' element={<OpenPage/>}/>
         <Route path='/create' element={<Register/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/main' element={<OpenPage/>}/>
+
       </Routes>
     </Router>
   );
---
+++ src/main.js
@@ -0,0 +1,10 @@
+function MainPage() {
+    
+    return (
+        <div>
+            Welcome
+        </div>
+    )
+}
+
+export default MainPage;
