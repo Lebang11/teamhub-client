1304cbe0084ef31eddc684b6646d29a3eed7a9e6
lebang11 <lebangnong@gmail.com>
2023-09-19 21:42:22 +0200

    trying to fetch from api

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 function MainPage() {
     
+    fetch('https://team-hub.onrender.com/api/main')
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
     return (
         <div>
             Welcome
