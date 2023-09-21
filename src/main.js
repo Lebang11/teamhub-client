85ea0b6de7b971a50f04402a195ba954c90b5eb2
L.Nong <lnong023@student.wethinkcode.co.za>
2023-09-21 10:55:11 +0200

    can view email, name and id

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import Cookies from "js-cookie";
 function MainPage() {
     const [info, setInfo] = useState('Details')
 
-    const getInfo = async () => {
+    const getInfo = async (data) => {
         const info = await axios.get('https://team-hub.onrender.com/api/main');
-        console.log(Cookies.get('token_name'))
-        setInfo(Cookies.get('token_name'))
+        console.log(Cookies.get(`token_${data}`))
+        setInfo(Cookies.get(`token_${data}`))
     }
     
 
@@ -17,9 +17,16 @@ function MainPage() {
             <div>
                 Welcome
             </div>
-            <button onClick={getInfo} className="user button">
-                User details
+            <button onClick={(e) => {getInfo('name')}} className="user button">
+                User name
             </button>
+            <button onClick={(e) => {getInfo('email')}} className="user button">
+                User email
+            </button>
+            <button onClick={(e) => {getInfo('id')}} className="user button">
+                User id
+            </button>
+
             <p>{info}</p>
         </div>
     )
