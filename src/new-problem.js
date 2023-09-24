8fbea23adc6197c6d66de22cb6f1068867313925
lebang11 <lebangnong@gmail.com>
2023-09-24 23:49:16 +0200

    some fixes and added firebase

--- src/new-problem.js
+++ src/new-problem.js
@@ -1,11 +1,41 @@
 import Cookies from "js-cookie";
 import axios from "axios";
 import { useState } from "react";
+import {storage} from "./firebase";
+import {getDownloadURL, ref, uploadBytes} from "firebase/storage";
+import { v4 } from 'uuid';
 
 const NewProblem = () => {
     // const [text, setText] = useState('');
     const [done, setDone] = useState('Done');
+    const [filename, setFileName] = useState('');
+    const [selectedFile, setSelectedFile] = useState();
+    const [Url, setURL] = useState('');
+    const [Ref, setRef] = useState('');
     const today = new Date();
+
+    const handleFileUpload = async () => {
+        // const file = selectedFile;
+        // // setFileName(file.name)
+        // // const formData = new FormData();
+        // // formData.append('file', file);
+        // // await axios
+        // // .post("https://team-hub.onrender.com/api/problems", formData, {
+        // //   headers: {
+        // //     "Content-Type": "multipart/form-data",
+        // //     "x-rapidapi-host": "file-upload8.p.rapidapi.com",
+        // //     "x-rapidapi-key": "your-rapidapi-key-here",
+        // //   },
+        // // })
+        // // .then((res) => console.log('file sent successfully'))
+        // // .catch(err=> console.log(err))
+
+        // let Ref = await ref(storage, `files/${v4()}-${selectedFile.name}`);        
+        // uploadBytes(Ref, selectedFile).then(()=> {
+        //     alert('Uploaded')
+        // });
+        // console.log(Ref.name)
+    }
 
     const createProblem= async () => {
         const author = Cookies.get('token_name');
@@ -15,27 +45,34 @@ const NewProblem = () => {
         const title = titleName.value;
         const languageName = document.getElementById('language');
         const language = languageName.value;
-        const date = `${today.getDate()} ${today.toLocaleString('default', { month: 'long' })} ${today.getFullYear()}, ${today.getHours()}:${today.getMinutes()}`;
-
+        const date = `${today.getDate()} ${today.toLocaleString('default', { month: 'long' })} ${today.getFullYear()}, ${today.getHours()}:${today.getMinutes()}`;        
+        
+        console.log(author);
         console.log(title);
         console.log(text);
-        console.log(author);
         console.log(language);
         console.log(date);
+        console.log(filename);
 
         setDone('Loading...');
+
+        // handleFileUpload();
         
         await axios.post('https://team-hub.onrender.com/api/problems',
         {
             author,
-            text,
             title,
+            text,
             language,
-            date
+            date,
+            filename
         }).then((res)=> console.log('Posted!', 'by', author)).catch(err=>console.log(err));
         setDone('Done')
         clearText()
+    
+        
     }
+
 
     function clearText() {
         const textarea = document.querySelector('.new-blog-text-box');
@@ -61,6 +98,12 @@ const NewProblem = () => {
                 <div>
                     <textarea name="blog" placeholder="text here" className="new-blog-text-box" ></textarea>
                 </div>
+                <div>
+                    <input type="file" onChange={(e) => {
+                        setSelectedFile(e.target.files[0])
+                        setFileName(e.target.files[0].name)
+                    }} />
+                </div>
                 <div className="language-div">
                     <label>Pick a language:_ 
                         <select id="language" defaultValue="python">
@@ -75,6 +118,15 @@ const NewProblem = () => {
             <div>
                 <button className="done-button" onClick={createProblem}>{done}</button>
                 <button className="done-button clear-button" onClick={clearText}>clear</button>
+            </div>
+            <div>
+                <button onClick={() => {
+                    getDownloadURL(Ref).then(url => {
+                        console.log(Url)
+                        setURL(Url)})
+                }}>
+                    {Url } 
+                </button>
             </div>
         </div>
     );
