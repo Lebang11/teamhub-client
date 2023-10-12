41dc3ed7cafd5d2d2bfe04a15c76dc8b1ca885f1
L.Nong <lnong023@student.wethinkcode.co.za>
2023-10-12 12:15:25 +0200

    done with reset page

--- src/reset.js
+++ src/reset.js
@@ -13,33 +13,21 @@ function ResetPage() {
     const [error, setError] = useState('')
 
 
-    const sendEmail = async () => {
 
-    
-        await axios.post(`https://team-hub.onrender.com/api/email`,
-        {
-            id,
-            password
+    const updatePassword = async () => {
+        if (password.length <5) {
+            setError('Password too short (5 letters or more).')
+        } else if (password !== passwordConfirm) {
+            setError('Passwords do not match')
+        } else {
+            await axios.post(`https://team-hub.onrender.com/api/user`, 
+            {
+                id,
+                password
+            })
         }
-        )
-        .then(res => {
-            console.log(res)
-            console.log('Email sent')
-        })
-        .catch(err => console.log(err))
-
-    // const updatePassword = async () => {
-    //     await axios.post(`https://team-hub.onrender.com/api/user`, 
-    //     {
-    //         _id,
-    //         password
-    //     })
-    // }
-
-
-
-  
-  }
+        
+    }
 
 
   return (
@@ -49,17 +37,14 @@ function ResetPage() {
               <div className='mb-3'>
                 <label for='password' className='form-label'>New Password</label>
                 <input className='form-control' type='password' onChange={(e) => setPassword(e.target.value)} value={password}></input>
-                {/* <div id="emailHelp" class="form-text">We'll send a link to reset password to this email.</div> */}
-                <small className='text-danger'>{error}</small>
               </div>
               <div className='mb-3'>
                 <label for='password' className='form-label'>Confirm Password</label>
                 <input className='form-control' type='password' onChange={(e) => setPasswordConfirm(e.target.value)} value={passwordConfirm}></input>
-                {/* <div id="emailHelp" class="form-text">We'll send a link to reset password to this email.</div> */}
                 <small className='text-danger'>{error}</small>
               </div>
 
-              <button type="button" class="btn btn-primary" >Submit</button>
+              <button type="button" class="btn btn-primary" onClick={updatePassword}>Submit</button>
             </form>
         </div>
      
